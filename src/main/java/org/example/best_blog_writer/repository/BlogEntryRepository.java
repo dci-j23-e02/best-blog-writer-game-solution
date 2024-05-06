5ea26567e284394d00a0fb8b9cb764218d8fd78a
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-05-06 10:02:33 +0200

    init commit

--- src/main/java/org/example/best_blog_writer/repository/BlogEntryRepository.java
+++ src/main/java/org/example/best_blog_writer/repository/BlogEntryRepository.java
@@ -4,5 +4,5 @@ import org.example.best_blog_writer.model.BlogEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlogEntryRepository extends JpaRepository<BlogEntry, Long> {
-
+// custom operation
 }
